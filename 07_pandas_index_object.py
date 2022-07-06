963919e14b445f151b7168fc13db58ad0bd9b488
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-06 21:46:26 +0200

    Update 07_pandas_index_object.py

--- 07_pandas_index_object.py
+++ 07_pandas_index_object.py
@@ -8,18 +8,18 @@
 import pandas as pd
 
 # Data
-index = pd.Index([1, 2, 3, 4, 5, 6, 7])
-index
+index = pd.Index([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+print(index)
 
 # [start:stop:step]
-index[1]        # Get the second element
-index[::2]      # Get every second element
+print(index[1])        # Get the second element
+print(index[::3])      # Get every third element
 
-indexA = pd.Index([1, 2, 3, 5, 8])
-indexB = pd.Index([2, 3, 5, 6, 10])
+indexA = pd.Index([1, 2, 3, 5, 8, 15, 29])
+indexB = pd.Index([2, 3, 5, 6, 10, 17, 30])
 
-indexA & indexB # Intersection
+indexA.__and__(indexB) # Intersection
 
-indexA | indexB # Union
+indexA.__or__(indexB)  # Union
 
-indexA ^ indexB # Symmetric Difference
+indexA.__xor__(indexB) # Symmetric Difference
