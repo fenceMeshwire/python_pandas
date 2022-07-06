352d1414669ce04896a1cc5c516b355c68eb621c
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-06 21:34:49 +0200

    Create 10_pandas_df_align_index.py

---
+++ 10_pandas_df_align_index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 10_pandas_df_align_index.py
+
+# Dependency
+import pandas as pd
+import numpy as np
+
+data = np.random.RandomState(15)      # Generate a seed of same results.
+matrix_A = pd.DataFrame(data.randint(0, 50, (2, 2)), columns=['n1', 'n2'], index=['m1', 'm2'])
+print(matrix_A)
+
+matrix_B = pd.DataFrame(data.randint(0, 30, (3, 3)), columns=['n1', 'n2', 'n3'], index=['m1', 'm2', 'm3'])
+print(matrix_B)
+
+mean_value = matrix_A.stack().mean()
+
+Matrix_C = matrix_A.add(matrix_B, fill_value=mean_value)
+print(Matrix_C)
+
+Matrix_D = matrix_B.add(matrix_A, fill_value=mean_value)
+print(Matrix_D)
+
+print(Matrix_C == Matrix_D) # Returns True
+
+# Other operations are possible:
+# sub, mul, div, mod, floordiv, pow
