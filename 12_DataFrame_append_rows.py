43588d572eb8f324d5fa7c1b1011617ad4e50539
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-21 00:07:36 +0200

    Create 12_DataFrame_append_rows.py
    Build a Pandas DataFrame object from scratch.

---
+++ 12_DataFrame_append_rows.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 12_DataFrame_append_rows.py
+
+# Dependency
+import pandas as pd
+
+heading = ["City", "Country Code", "Inhabitants", "Latitude, Longitude"]
+
+df = pd.DataFrame(columns=heading)
+df.loc[len(df.index)] = ['Munich', 'DE', 1.472, (48.137154, 11.576124)]
+df.loc[len(df.index)] = ['Berlin', 'DE', 3.645, (52.520008, 13.404954)]
+df.loc[len(df.index)] = ['Copenhagen', 'DK', 0.602, (55.676098, 12.568337)]
+
+print(df)
