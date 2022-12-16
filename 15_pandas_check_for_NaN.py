06933583772299825fbb719686798cf267ec6e33
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-12-17 00:01:18 +0100

    Create 15_pandas_check_for_NaN.py

---
+++ 15_pandas_check_for_NaN.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 15_pandas_check_for_NaN.py
+
+# Dependencies
+import pandas as pd
+import os, platform
+from pathlib import Path
+
+if os.name == 'nt' or platform.system() == 'Windows':
+    path = 'C:\\...'
+    os.chdir(path)
+if os.name == 'posix' or platform.system() == 'Darwin':
+    path = '/Users/...'
+    os.chdir(path)
+
+print('Path changed to:', Path.cwd())
+
+df = pd.read_csv('file.csv')
+df.head() # Check the first five rows
+df.info() # Check the DataFrame information
+
+# Check for missing data:
+df.isnull().values.any()
+
+# Identify the number of missing data elements:
+for col_name in df.columns.values:
+    value = df[col_name].isna().sum()
+    if value > 0:
+        print(col_name, value)
