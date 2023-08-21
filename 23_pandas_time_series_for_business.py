9cd8a1df2115473d36761b0b1185eed93ef032ed
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-08-21 23:02:57 +0200

    Update 23_pandas_time_series_for_business.py

--- 23_pandas_time_series_for_business.py
+++ 23_pandas_time_series_for_business.py
@@ -7,7 +7,7 @@
 # Dependencies
 import pandas as pd
 
-pd.period_range('2023-07-03', periods=8, freq='B')  # Returns Business Days
+pd.date_range('2023-01-03', periods=8, freq='B')  # Returns Business Days
 pd.date_range('2023-01-01', periods=12, freq='BM')  # End of Business Month
 pd.date_range('2023-01-01', periods=12, freq='BQ')  # End of Business Quarter
 pd.date_range('2023-01-01', periods=1, freq='BA')   # End of Business Year
